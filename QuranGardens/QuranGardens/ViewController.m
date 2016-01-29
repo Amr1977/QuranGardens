422a16f5c90091d3e8c0c95c16e67e190dbe76fe
Amr Lotfy <amr.lotfy@badrit.com>
2016-01-29 11:44:05 +0200

    first commit

---
+++ QuranGardens/QuranGardens/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QuranGardens
+//
+//  Created by Amr Lotfy on 1/29/16.
+//  Copyright © 2016 Amr Lotfy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

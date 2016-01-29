6b058ee5a5876fe0121886066609a067418ca86b
Amr Lotfy <amr.lotfy@badrit.com>
2016-01-29 13:36:14 +0200

    Use Collection view controller
    SO: http://stackoverflow.com/questions/17856055/creating-a-uicollectionview-programmatically

--- QuranGardens/QuranGardensViewController.h
+++ QuranGardens/QuranGardensViewController.h
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface QuranGardensViewController : UIViewController
-
-
+@interface QuranGardensViewController : UICollectionViewController <UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
 
 @end
--- QuranGardens/QuranGardensViewController.m
+++ QuranGardens/QuranGardensViewController.m
@@ -11,8 +11,51 @@
 
 @interface QuranGardensViewController ()
 
+
+
 @end
 
 @implementation QuranGardensViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    //[self.collectionView setContentInset:UIEdgeInsetsMake(200, 100, 100, 100)];
+    
+    UICollectionViewFlowLayout *layout=[[UICollectionViewFlowLayout alloc] init];
+    self.collectionView=[[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
+    [self.collectionView setDataSource:self];
+    [self.collectionView setDelegate:self];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellIdentifier"];
+    [self.collectionView setBackgroundColor:[UIColor blackColor]];
+    
+   // [self.view addSubview:_collectionView];
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 114;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:@"cellIdentifier" forIndexPath:indexPath];
+    
+    cell.backgroundColor=[UIColor colorWithWhite:255/255 alpha:0.5];
+    return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(100, 100);
+}
 
 @end
